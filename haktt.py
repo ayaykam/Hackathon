ea7b319d0bcaac421ce034e6b59035924a87af5b
ayaykam <152839701+ayaykam@users.noreply.github.com>
2023-12-14 16:19:36 +0600

    Update haktt.py

--- haktt.py
+++ haktt.py
@@ -10,33 +10,35 @@ def write_to_csv(data: dict):
 
 def get_html(url):
     response = requests.get(url)
+    # print(response.status_code)
     return response.text
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    cars = soup.find_all('div', class_='name')
+    cars = soup.find_all('div', class_='table-view-list')
 
     for car in cars:
         try:
-            title = car.find('div', class_='table-view clr label-viez').find('a').text
+            title = car.find('div', class_='title').find('h2').text.strip()
         except:
             title = ''
 
         try:
-            price = car.find('div', class_='table-view clr label-view').find('strong').text
+            price = car.find('div', class_='price').find('p').find('strong').text.split()
+            price = ' '.join(price)
         except:
             price = ''
 
         try:
-            desc = car.find('div', class_='block info-wrapper item-info-wrapper').find_all('p')
-            desc = ' '.join(p.text for p in desc)
+            desc = car.find('div', class_='block info-wrapper item-info-wrapper').text.split()
+            desc = ' '.join(desc)
         except:
             desc = ''
 
         try:
-            img = car.find('img').get('src')
+            img = car.find('div', class_='thumb-item-carousel').find('img').attrs.get('src')
         except:
-            img = ''
+            img = 'none image'
 
         data = {
             'title': title,
@@ -49,22 +51,27 @@ def get_data(html):
 
 def get_page(html):
     soup = BeautifulSoup(html, 'lxml')
-    pages = soup.find('ul', class_='pagination').find_all('li')
-    last_page = pages[-1].find('a').get('href').split('=')[-1]
-    return last_page
+    page_list = soup.find('ul', class_='pagination').find_all('a')[-1].attrs.get('data-page')
+    if page_list is not None:
+        # print(page_list)
+        return int(page_list)
+    return 0
 
 
 def main():
     url = 'https://www.mashina.kg/search/all/'
     html= get_html(url)
-    page = get_page(html)
-
-    for page_num in range(1, int(page) + 1):
-        page_url = f'{url}?page={page_num}'
-        html = get_html(page_url)
+    num = get_page(html)
+    for i in range(1, num+1):
+        # print(i)
+        url_page = url + '?page=' + str(i)
+        print(url_page)
+        html = get_html(url_page)
         get_data(html)
+        print(f'sparsing {i} - машина')
 
-with open('data.csv', 'w', newline='') as file:
+
+with open('data.csv', 'w') as file:
     write = csv.writer(file)
     write.writerow(['title', 'price', 'image', 'description'])
 
